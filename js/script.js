e7ccefc7ccd8762015ab67258d5f2f6db9f2e8ba
riskiai <riskiahmadilham2002@gmail.com>
2023-10-10 16:09:13 +0700

    update section style header interactive JS

--- js/script.js
+++ js/script.js
@@ -18,5 +18,26 @@
         navlist.classList.remove('active');
     }
 
+    /* Scroll Animasi */
+    let sections = document.querySelectorAll('section')
+    let navLinks = document.querySelectorAll('header ul li a')
+
+    window.onscroll = () => {
+        sections.forEach(sec => {
+            let top = window.scrollY;
+            let offset = sec.offsetTop - 100;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute('id')
+
+            if(top >= offset && top < offset + height ){
+                //active navbar links
+                navLinks.forEach(links => {
+                    links.classList.remove('active');
+                    document.querySelector('header ul li a[href*='+ id + ']').classList.add('active')
+             });
+        }
+    });
+    }
+
 
 
